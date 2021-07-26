df3e72ab2c04c70427a9a73ed2ae6ef481407903
Al-mamun <almamun.f.f.f@gmail.com>
2021-07-26 18:31:29 +0600

    commited1

---
+++ app.js
@@ -0,0 +1,23 @@
+let maxAttempts = 4;
+
+let btn = document.getElementById("btn");
+let output = document.getElementById("outputtext");
+
+let number = [Math.floor(Math.random() * 100)];
+
+btn.addEventListener("click", function () {
+  let input = document.getElementById("userInput").value;
+  if (input == number) {
+    output.innerHTML = `You guessed right, your number was ${number}`;
+  } else if (input < number) {
+    output.innerHTML =
+      "You guessed too low! & You have " + maxAttempts + " attempts left";
+    maxAttempts -= 1;
+  } else if (input > number) {
+    output.innerHTML =
+      "You guessed too high! & You have " + maxAttempts + " attempts left";
+    maxAttempts -= 1;
+  } else if (maxAttempts === 4) {
+    output.innerHTML = "You weren't able to guess the correct number";
+  }
+});
